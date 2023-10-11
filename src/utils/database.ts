406828b1dbb55b62c2920d83ae88c4e9e0588e7b
Innocent <innocentanyaele2000@gmail.com>
2023-10-11 10:46:40 +0000

    switched to elephant ql

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -5,16 +5,11 @@ import { createPostTableQuery, createUserTableQuery } from './queries';
 dotenv.config()
 
 const pool = new Pool({
-    // host: process.env.DB_HOST,
-    // port: Number(process.env.DB_PORT),
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'admin',
-    database: 'blog'
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 const createTables = async () => {
